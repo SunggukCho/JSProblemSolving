2b9b7fd980db0c626da3937ee5f13b076778339c
likelionSungGuk <sung9gi@gmail.com>
2022-04-12 23:32:48 +0900

    0412 문자열압축

---
+++ 문자열압축.js
@@ -0,0 +1,16 @@
+// https://programmers.co.kr/learn/courses/30/lessons/60057?language=javascript
+
+function solution(s) {
+    var answer = 0;
+    
+
+
+
+
+    return answer;
+}
+solution("aabbaccc"); // 7
+solution("ababcdcdababcdcd"); // 9
+solution("abcabcdede"); // 8
+solution("abcabcabcabcdededededede"); // 14
+solution("xababcdcdababcdcd"); // 17
