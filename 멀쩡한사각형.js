487e93008780d0020856322aab050d9cdc0c8a83
likelionSungGuk <sung9gi@gmail.com>
2022-04-16 22:51:58 +0900

    0416 멀쩡한 사각형

---
+++ 멀쩡한사각형.js
@@ -0,0 +1,6 @@
+// https://programmers.co.kr/learn/courses/30/lessons/62048?language=javascript
+
+function solution(w, h) {
+    var answer = 1;
+    return answer;
+}
--- 문자열압축.js
+++ 문자열압축.js
@@ -1,16 +1,23 @@
 // https://programmers.co.kr/learn/courses/30/lessons/60057?language=javascript
 
 function solution(s) {
-    var answer = 0;
+    let answer = 0;
+    let length = s.length;
+    const stringArray = s.split('');
+    for (let step = 1; step <= length; step++) {
+        for (let i = 0; i < length/step ;i++) {
+            const block = stringArray.slice(i, i+step)
+            console.log(step, block)
+        }
+    }
     
-
 
 
 
     return answer;
 }
 solution("aabbaccc"); // 7
-solution("ababcdcdababcdcd"); // 9
-solution("abcabcdede"); // 8
-solution("abcabcabcabcdededededede"); // 14
-solution("xababcdcdababcdcd"); // 17
+// solution("ababcdcdababcdcd"); // 9
+// solution("abcabcdede"); // 8
+// solution("abcabcabcabcdededededede"); // 14
+// solution("xababcdcdababcdcd"); // 17
