2073b86e0a071e8bffeda34400b97be200ba1205
likelionSungGuk <sung9gi@gmail.com>
2022-04-17 23:09:23 +0900

    0416 멀쩡한사각형

--- 멀쩡한사각형.js
+++ 멀쩡한사각형.js
@@ -1,6 +1,11 @@
 // https://programmers.co.kr/learn/courses/30/lessons/62048?language=javascript
 
-function solution(w, h) {
-    var answer = 1;
-    return answer;
-}
+function solution(w,h) {
+    let c = 0; //cross
+    for(let i=0; i<w; i++){
+      c = Math.ceil((h*(i+1))/w)-Math.floor((h*i)/w)+c;
+    }
+    return w*h-c
+  }
+
+solution(8, 12) // 80
