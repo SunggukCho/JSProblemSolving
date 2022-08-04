cdfa919356b92413209a8354b10a2c38c530fe5c
likelionSungGuk <sung9gi@gmail.com>
2022-08-04 23:34:52 +0900

    fix: BOJ 1260 DFS와 BFS

--- BOJ/1260DFS와BFS.js
+++ BOJ/1260DFS와BFS.js
@@ -1,3 +1,5 @@
+const { getEnabledCategories } = require("trace_events");
+
 // let input = require('fs').readFileSync('/dev/stdin').toString().split(/\r?\n/)
 let input = `4 5 1
 1 2
@@ -10,9 +12,21 @@ const [firstrow, ...graph] = input;
 const [N, M, V] = firstrow.map(a => parseInt(a))
 const nodes = graph.map(edge => edge.map(e => parseInt(e)))
 
-function DFS(v) {
-  
+let G = Array.from(Array(N+1), ()=> new Array(N+1).fill(0))
+nodes.forEach(node => {
+  const [a, b] = node
+  G[a][b] = 1
+  G[b][a] = 1
+})
+console.log(G)
+let visited = []
+function DFS(v, visited) {
+  visited.push(v)
+  console.log(v)
+  for (let i = 0; i < N+1; i++) {
+    if (G[v][i] === 1 && !visited.includes(i)) {
+      DFS(i, visited)
+    }
+  }
 }
-function BFS(v) {
-
-}
+DFS(V, visited)
--- DFS로부분집합구하기.js
+++ DFS로부분집합구하기.js
@@ -1,7 +1,20 @@
 // 자연수 N이 주어지면 1부터 N까지의 원소를 갖는 집합의 부분집합을 모두 출력하는 프로그램을 작성하세요. 
 
-let input = 3
+let N = 3
 
-function DFS(l) {
-  
+let check = Array.from({length: N+1}, ()=> 0)
+
+let stack = [];
+function DFS(v) {
+  if (v === N+1) {
+    for (let i = 1; i <= N; i++) {
+      if (check[i] === 1) console.log(i)
+    }
+  } else {
+    check[v] = 1
+    DFS(v+1)
+    check[v] = 0
+    DFS(v+1)
+  }
 }
+DFS(1)
