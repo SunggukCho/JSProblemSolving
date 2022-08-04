2f4c12a54da61a07f8c83df52b8c000f69f8ab3c
likelionSungGuk <sung9gi@gmail.com>
2022-08-05 00:23:55 +0900

    fix: BOJ 1260 DFS와 BFS 3rd

--- BOJ/1260DFS와BFS.js
+++ BOJ/1260DFS와BFS.js
@@ -1,12 +1,12 @@
-const { getEnabledCategories } = require("trace_events");
 
 // let input = require('fs').readFileSync('/dev/stdin').toString().split(/\r?\n/)
-let input = `4 5 1
-1 2
-1 3
-1 4
-2 4
-3 4`
+let input = require('fs').readFileSync('./file/1260.txt').toString()
+// let input = `4 5 1
+// 1 2
+// 1 3
+// 1 4
+// 2 4
+// 3 4`
 input = input.split(/\r?\n/).map(a => a.split(" "))
 const [firstrow, ...graph] = input;
 const [N, M, V] = firstrow.map(a => parseInt(a))
@@ -18,15 +18,41 @@ nodes.forEach(node => {
   G[a][b] = 1
   G[b][a] = 1
 })
-console.log(G)
 let visited = []
+let dfs = ""
+let bfs = ""
 function DFS(v, visited) {
   visited.push(v)
-  console.log(v)
+  dfs+= `${v} `
   for (let i = 0; i < N+1; i++) {
     if (G[v][i] === 1 && !visited.includes(i)) {
       DFS(i, visited)
     }
   }
 }
+let queue = []
+let visitedBFS = []
+function BFS(start, queue, visitedBFS) {
+  queue.push(start)
+  visitedBFS.push(start)
+  while (queue.length) {
+    let v = queue.shift()
+    bfs+= `${v} `
+    if (v === undefined) break
+    for (let i = 0; i < N+1; i++) {
+      if (G[v][i] === 1 && !visitedBFS.includes(i)) {
+        console.log(v, i, queue, visitedBFS)
+        queue.push(i)
+        visitedBFS.push(i)
+      }
+    }
+  }
+}
+
+
 DFS(V, visited)
+BFS(V, queue, visitedBFS)
+const dfsLen = dfs.length
+const bfsLen = bfs.length
+console.log(dfs.slice(0, dfsLen-1))
+console.log(bfs.slice(0, bfsLen-1))
