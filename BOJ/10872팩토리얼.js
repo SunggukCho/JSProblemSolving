b57674d6af7ef34055d7e3d1829d74054ae0fde1
likelionSungGuk <sung9gi@gmail.com>
2022-08-09 22:58:15 +0900

    add:BOJ 10872 팩토리얼

---
+++ BOJ/10872팩토리얼.js
@@ -0,0 +1,17 @@
+// let input = require('fs').readFileSync('/dev/stdin').toString()
+let input = require('fs').readFileSync('./file/10872.txt').toString()
+const N = parseInt(input)
+
+let memo = [1, 1]
+function F(N) {
+  if (memo[N] === undefined) {
+    for (let i = 1; i < N; i++) {
+      memo[i+1] = memo[i]*(i+1)
+    }
+    return memo[N]
+  } else {
+    return memo[N]
+  }
+}
+let answer = F(N)
+console.log(answer)
