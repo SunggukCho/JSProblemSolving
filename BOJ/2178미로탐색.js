2a7a8bb4cb9dbfcc9c63a3b6fade1567f735bcec
likelionSungGuk <sung9gi@gmail.com>
2022-08-11 23:56:59 +0900

    fix: BOJ 2178미로탐색

--- BOJ/2178미로탐색.js
+++ BOJ/2178미로탐색.js
@@ -3,10 +3,34 @@ let input = require('fs').readFileSync('./file/2178.txt').toString().split(/\r?\
 const [N, M] = input.shift().split(" ").map(a => parseInt(a))
 const newInput = input.map(i => i.split("").map(b => parseInt(b)))
 
-let visited = Array.from({length: M}, ()=>Array(N).fill(0))
-console.log(newInput)
-console.log(visited)
-function BFS (startX, startY, visited) {
+let visited = Array.from({length: N}, ()=>Array(M).fill(0))
+let answer = 0;
+
+const dx = [-1, 0, 1, 0]
+const dy = [0, 1, 0, -1]
+function BFS (startX, startY) {
+  let queue = []
+  let level = 1;
+  queue.push([startX, startY, level])
+  while (queue.length > 0) {
+    const [x, y, l] = queue.shift()
+    
+    visited[x][y] = l
+    if (x === N-1 && y === M-1) {
+      console.log(visited[N-1][M-1])
+      break
+    }
+    for (let i = 0; i < 4; i ++) {
+      // 4방 탐색
+      const nx = x+dx[i]
+      const ny = y+dy[i]
+      if (nx >= 0 && nx < N && ny >= 0 && ny < M && newInput[nx][ny] === 1 && visited[nx][ny] === 0) {
+        // true
+        let tmp = visited[x][y]
+        queue.push([nx, ny, tmp+1])
+      }
+    }
+  }
 
 }
-BFS(0, 0, visited)
+BFS(0, 0)
