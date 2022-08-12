4fe8a71512e790fc465f732a4e3e4ba34d0135bd
likelionSungGuk <sung9gi@gmail.com>
2022-08-12 23:35:07 +0900

    add: BOJ 2293 동전

--- BOJ/2178미로탐색.js
+++ BOJ/2178미로탐색.js
@@ -4,7 +4,6 @@ const [N, M] = input.shift().split(" ").map(a => parseInt(a))
 const newInput = input.map(i => i.split("").map(b => parseInt(b)))
 
 let visited = Array.from({length: N}, ()=>Array(M).fill(0))
-let answer = 0;
 
 const dx = [-1, 0, 1, 0]
 const dy = [0, 1, 0, -1]
@@ -14,7 +13,6 @@ function BFS (startX, startY) {
   queue.push([startX, startY, level])
   while (queue.length > 0) {
     const [x, y, l] = queue.shift()
-    
     visited[x][y] = l
     if (x === N-1 && y === M-1) {
       console.log(visited[N-1][M-1])
@@ -24,13 +22,12 @@ function BFS (startX, startY) {
       // 4방 탐색
       const nx = x+dx[i]
       const ny = y+dy[i]
-      if (nx >= 0 && nx < N && ny >= 0 && ny < M && newInput[nx][ny] === 1 && visited[nx][ny] === 0) {
+      if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue; 
+      if (newInput[nx][ny] === 1 && visited[nx][ny] === 0) {
         // true
-        let tmp = visited[x][y]
-        queue.push([nx, ny, tmp+1])
+        queue.push([nx, ny, l+1])
       }
     }
   }
-
 }
 BFS(0, 0)
