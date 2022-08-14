3d86c4f7f15e302e38abd11b27956cb441c2dfd9
likelionSungGuk <sung9gi@gmail.com>
2022-08-14 23:26:14 +0900

    add: BOJ 17135캐슬디펜스

---
+++ BOJ/17135캐슬디펜스.js
@@ -0,0 +1,3 @@
+// let input = require('fs').readFileSync('/dev/stdin').toString().split(/\r?\n/)
+let input = require('fs').readFileSync('./file/17135.txt').toString().split(/\r?\n/)
+const [N, ...rest] = input
