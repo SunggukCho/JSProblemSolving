ac8dd709d15a244acb6c6c1d7ee57189cfd0847a
likelionSungGuk <sung9gi@gmail.com>
2022-08-26 23:24:47 +0900

    add programmers 문자열압축

--- 프로그래머스/문자열압축.js
+++ 프로그래머스/문자열압축.js
@@ -2,19 +2,42 @@
 
 function solution(s) {
     let answer = 0;
-    let length = s.length;
-    const stringArray = s.split('');
-    for (let step = 1; step <= length; step++) {
-        for (let i = 0; i < length/step ;i++) {
-            const block = stringArray.slice(i, i+step)
-            console.log(step, block)
+    let left = '';
+    let right = '';
+    let result = [];
+  
+    for (let i = 1; i < Math.ceil(s.length / 2) + 1; i++) {
+        result.push([]);
+        // i = length
+        let cnt = 1;
+        for (let k = 0; k < s.length; k++) {
+            left = s.substr(k * i, i);
+            right = s.substr((k * i) + i, i);
+            
+            if (left === right) {
+                cnt += 1;
+            }
+            else {
+                // 똑같은 문자열이 있는 경우 숫자와 문자열을 같이 저장
+                if (cnt > 1) {
+                    result[i - 1] += String(cnt) + left;
+                }
+                // 똑같은 문자열이 없는 경우 문자열만 저장
+                else {
+                    result[i - 1] += left;
+                }
+                cnt = 1;
+            }
         }
     }
     
-
-
-
-    return answer;
+    let min = result[0].length;
+    for (let i in result) {
+        if (min > result[i].length) {
+            min = result[i].length;
+        }
+    }
+    return min;
 }
 solution("aabbaccc"); // 7
 // solution("ababcdcdababcdcd"); // 9
---
+++ 프로그래머스/숫자 문자열과 영단어.js
@@ -0,0 +1,22 @@
+// 2021 카카오 채용연계형 인턴십
+
+function solution(s) {
+  var answer = 0;
+  const numMap = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  }
+
+
+
+
+  return answer;
+}
