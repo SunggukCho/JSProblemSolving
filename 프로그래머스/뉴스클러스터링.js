48820dfad92669769c966a0672cdef01c00ae0cc
likelionSungGuk <sung9gi@gmail.com>
2022-08-31 23:47:10 +0900

    add: programmers 가장큰수

--- 프로그래머스/뉴스클러스터링.js
+++ 프로그래머스/뉴스클러스터링.js
@@ -6,14 +6,61 @@
 // 4. A & B 비교
 
 const regex = /[^a-zA-Z]/g // 알파벳이 아닌것
-function solution(str1, str2) {
-  var answer = 0;
-  const test = str1.replace(regex, "").toLowerCase();
-  const test2 = str2.replace(regex, "").toLowerCase();
+// function solution(str1, str2) {
+//   var answer = 0;
+//   const string1 = str1.replace(regex, "").toLowerCase();
+//   const string2 = str2.replace(regex, "").toLowerCase();
+//   const arr1 = [];
+//   const arr2 = [];
+//   for (let i = 0 ; i < string1.length-1; i++) {
+//     arr1.push(string1.slice(i,i+2))
+//   }
+//   for (let i = 0 ; i < string2.length-1; i++) {
+//     arr2.push(string2.slice(i,i+2))
+//   }
+//   let union = 0
+//   let intersection = 0
+//   const sum = [...new Set([...arr1, ...arr2])]
+//   sum.forEach(item => {
+//     const has1 = arr1.filter(x => x === item).length;
+//     const has2 = arr2.filter(x => x === item).length;
+//     union += Math.max(has1, has2);
+//     intersection += Math.min(has1, has2);
+//   })
+  
+//   return union === 0 ? 65536 : Math.floor(intersection / union * 65536)
+// }
 
+function solution (str1, str2) {
+  //
+  function explode(text) {
+    const result = [];
+    for (let i = 0; i < text.length - 1; i++) {
+      const node = text.substr(i, 2);
+      if (node.match(/[A-Za-z]{2}/)) {
+        result.push(node.toLowerCase());
+      }
+    }
+    return result;
+  }
 
-  return answer;
+  const arr1 = explode(str1);
+  const arr2 = explode(str2);
+  const set = new Set([...arr1, ...arr2]);
+  let union = 0;
+  let intersection = 0;
+  console.log(arr1, arr2, set)
+  //같은 원소를 검사해서 많은  쪽은 union에 더하고 적은쪽은 intersection에 더한다.
+  set.forEach(item => {
+    const has1 = arr1.filter(x => x === item).length;
+    const has2 = arr2.filter(x => x === item).length;
+    union += Math.max(has1, has2);
+    intersection += Math.min(has1, has2);
+  })
+  return union === 0 ? 65536 : Math.floor(intersection / union * 65536);
 }
 
 console.log(solution("FRANCE", "french"))
+console.log(solution("handshake", "shake hands"))
 console.log(solution("aa1+aa2", "AAAA12"))
+console.log(solution("E=M*C^2", "e=m*c^2"))
