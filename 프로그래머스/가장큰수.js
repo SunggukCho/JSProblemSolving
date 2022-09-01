bc63848ee623fc5090c9f0f89b84c7eef7dc2d20
likelionSungGuk <sung9gi@gmail.com>
2022-09-01 23:47:37 +0900

    add: programmers 쿼드압축후개수세기

--- 프로그래머스/가장큰수.js
+++ 프로그래머스/가장큰수.js
@@ -1,4 +1,31 @@
+// 실패
 function solution(numbers) {
-  var answer = '';
-  return answer;
+  const nLength = numbers.length;
+  const checked = Array.from({length: nLength}, ()=>0);
+  const tmp = Array.from({length: nLength}, ()=>0);
+  let maxNum = 0;
+  function DFS(v) {
+    if (v === nLength) {
+      const sum = tmp.reduce((prev, curr) => prev + curr)
+      maxNum = Math.max(maxNum, parseInt(sum))
+    } else {
+      for (let i = 0; i < nLength; i++) {
+        if (checked[i] === 0) {
+          checked[i] = 1
+          tmp[v] = String(numbers[i])
+          DFS(v+1)
+          checked[i] = 0
+        }
+      }
+    }
+  }
+  DFS(0)
+  return String(maxNum);
 }
+
+console.log(solution([6, 10, 2])) // "6210"
+console.log(solution([3, 30, 34, 5, 9])) // "9534330"
+console.log(solution([979, 97, 978, 81, 818, 817])) // "9799797881881817"
+console.log(solution([0,0,0,0])) 
+console.log(solution([70,0,0,0,0]))
+console.log(solution([232,23]))
---
+++ 프로그래머스/쿼드압축후개수세기.js
@@ -0,0 +1,10 @@
+// 월간 코드 챌린지 시즌1
+function solution(arr) {
+    var answer = [];
+    
+
+    return answer;
+}
+
+
+console.log(solution([[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]]))
