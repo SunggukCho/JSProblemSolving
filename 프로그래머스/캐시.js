c6c0f233d5c753720a0e6985c7416df90458727b
likelionSungGuk <sung9gi@gmail.com>
2022-09-05 23:53:31 +0900

    fix: programmers 캐시 반례 추가

--- 프로그래머스/캐시.js
+++ 프로그래머스/캐시.js
@@ -2,5 +2,29 @@
 
 function solution(cacheSize, cities) {
   var answer = 0;
+  let queue = [];
+  cities.forEach(city => {
+    if (queue.includes(city.toLowerCase())) {
+      const index = queue.findIndex((c)=> c === city.toLowerCase())
+      queue.splice(index,1)
+      queue.push(city.toLowerCase())
+      answer+=1
+    } else {
+      if (queue.length >= cacheSize && queue.length > 0) {
+        queue.shift()
+      } 
+      if (cacheSize > 0) {
+        queue.push(city.toLowerCase())
+      }
+      answer+=5
+    }
+  })
   return answer;
 }
+
+console.log(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"])) //50
+console.log(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"])) //21
+console.log(solution(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"])) // 60
+console.log(solution(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"])) // 50
+console.log(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"])) // 16
+console.log(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"])) // 25
