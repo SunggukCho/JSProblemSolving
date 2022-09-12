509255fa83b682f33d5a7287b26c3fb384168ba8
likelionSungGuk <sung9gi@gmail.com>
2022-09-12 15:45:44 +0900

    fix: programmers 124나라의숫자 - 효율성테스트 통과

--- 프로그래머스/124나라의숫자.js
+++ 프로그래머스/124나라의숫자.js
@@ -1,15 +1,16 @@
+/*
+ 3진법으로 변환하는 코드를 짠다는 개념. 0, 1, 2 => 1, 2, 4로 한 칸씩 땡겨짐
+ 하지만 0 이 없기 때문에 0으로 나눠 떨어진다면 4, 아니면 그 나머지를 인덱스로 가진 배열에서 불러오기
+*/
 function solution(n) {
-  const memo = ["1", "2", "4"]
-  const length = memo.length
-  const remainderConverter = ["1", "2", "4"]
-  for (let i = length; i <= n; i++) {
-    const [share, remainder] = [Math.floor(i/3), i%3]
-    const convertedRemainder = remainderConverter[remainder]
-    const addStringNum = String(memo[share-1])+convertedRemainder
-    memo[i] = addStringNum
+  var answer = ''
+  const remainderConverter = ["4", "1", "2"]
+  while (n >0) {
+    const remainder = n%3
+    answer = remainderConverter[remainder]+answer
+    n = Math.floor((n-1)/3)
   }
-  // console.log(memo)
-  return memo[n-1];
+  return answer;
 }
 
 console.log(solution(10))
