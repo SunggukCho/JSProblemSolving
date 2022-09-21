017426232528ab2f03db3f9e8c50e53b33e27eaa
likelionSungGuk <sung9gi@gmail.com>
2022-09-21 23:10:02 +0900

    add: programmers 멀리뛰기.js

---
+++ 프로그래머스/멀리뛰기.js
@@ -0,0 +1,20 @@
+/**
+ * 점화식 DP[n] = DP[n-1] + DP[n-2]
+ */
+function solution(n) {
+  const memo = [0, 1, 2]
+  if (n >= 2) {
+    for (let i = 3; i <= n; i++) {
+      memo[i] = (memo[i-1]+memo[i-2]) % 1234567
+    }
+  }
+  console.log(memo)
+  return memo[n] ;
+}
+
+
+console.log(solution(3)) // 3
+console.log(solution(4)) // 5
+console.log(solution(5)) // 3
+console.log(solution(6)) // 3
+console.log(solution(7)) // 3
