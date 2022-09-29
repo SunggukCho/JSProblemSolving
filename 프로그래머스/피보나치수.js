059c805de4fe9ad1806d0018ae37537f5442d749
likelionSungGuk <sung9gi@gmail.com>
2022-09-29 23:33:44 +0900

    add: 피보나치수

---
+++ 프로그래머스/피보나치수.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  var answer = 0;
+  const memo = [0, 1]
+  for (let i = 2; i <= n; i++) {
+    memo[i] = memo[i-2]+memo[i-1] % 1234567
+  }
+  answer = memo[n] 
+  return answer;
+}
+
+console.log(solution(2)) 
+console.log(solution(3)) // 2
+console.log(solution(4)) // 3
+console.log(solution(5)) // 5
